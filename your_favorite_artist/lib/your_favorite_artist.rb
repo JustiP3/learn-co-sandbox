0632d44f03ec95ddd1241a1993a7cb7314bd201c
Justin Phillips <justinphil02@gmail.com>
2020-11-16 03:56:45 +0000

    Automatically backed up by Learn

--- your_favorite_artist/lib/your_favorite_artist.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-require 'json'
-require 'httparty'
-
-require_relative 'your_favorite_artist/version'
-require_relative 'your_favorite_artist/CLI'
-require_relative 'your_favorite_artist/API'
-require_relative 'your_favorite_artist/artist'
-require_relative 'your_favorite_artist/album'
--- your_favorite_artist/lib/your_favorite_artist/version.rb
+++
@@ -1,3 +0,0 @@
-module YourFavoriteArtist
-  VERSION = "0.1.0"
-end
